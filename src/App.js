31bf593e70d9a9ee3763caef60bf4b227c9a243b
MMarek <mildnermarek@gmail.com>
2020-05-06 16:45:49 +0200

    styles + filter + include

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
         super();
 
         this.state = {
-            monsters: []
+            monsters: [],
+            searchField: ''
         };
     }
 
@@ -20,13 +21,23 @@ class App extends Component {
     }
 
     render() {
+      const { monsters, searchField } = this.state;
+      const filteredMonsters = monsters.filter(monster =>
+          monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
         return (
             <div className="App">
-              <CardList monsters={this.state.monsters}>
-              </CardList>
-            </div>
-        );
-    }
-}
+                <input
+                    type='search'
+                    placeholder='Search Monster'
+                    onChange={e => {
+                        this.setState({searchField: e.target.value});
+                    }}
+                        />
+                        <CardList monsters={filteredMonsters}/>
+                        </div>
+                        );
+                    }
+                }
 
-export default App;
+                export default App;
