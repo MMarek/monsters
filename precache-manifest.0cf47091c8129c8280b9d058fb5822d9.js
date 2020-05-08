96cdfb7960b1c153e69cef754290821620a0ee8e
MMarek <mildnermarek@gmail.com>
2020-05-08 12:54:17 +0200

    Updates

---
+++ precache-manifest.0cf47091c8129c8280b9d058fb5822d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3311d07061f5392107ecf2b90ba22c2c",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "342aa3e9313e1d7f566b",
+    "url": "/monsters/static/css/main.b19ce811.chunk.css"
+  },
+  {
+    "revision": "cb80a15f71da0f888782",
+    "url": "/monsters/static/js/2.668f8c51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.668f8c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "342aa3e9313e1d7f566b",
+    "url": "/monsters/static/js/main.32399274.chunk.js"
+  },
+  {
+    "revision": "3ad366ebbc5ea22c619f",
+    "url": "/monsters/static/js/runtime-main.735d0d8b.js"
+  }
+]);
